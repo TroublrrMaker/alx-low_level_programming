75ccab0200c1d61b4bca74ffaab2456cc7f56220
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-20 15:35:57 -0700

    Found it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-char message[]= "Programming is like building a multilingual puzzle";
+char message[]= "\"Programming is like building a multilingual puzzle";
 puts(message);
 return 0;
 } 
