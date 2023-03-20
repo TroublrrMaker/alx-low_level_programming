513073aa8c5c296139e00c0d7433c33f433dc468
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-20 16:04:30 -0700

    Azonto Ghosted

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
- char message[] = "\"Programming is like building a multilingual puzzle";
+puts(messge) = "\"Programming is like building a multilingual puzzle";
 
-puts(message);
 return (0);
 } 
