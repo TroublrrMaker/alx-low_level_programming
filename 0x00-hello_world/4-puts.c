3cd81c8b8ecc6dd9fe8fd85c7540ac8cc92400ae
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-20 16:08:19 -0700

    Tired.com

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-puts(messge) = "\"Programming is like building a multilingual puzzle";
+puts("\"Programming is like building a multilingual puzzle";)
 
 return (0);
 } 
