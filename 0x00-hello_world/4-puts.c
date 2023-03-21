ff057efbff9213dd8a6ff4a169a48677fd6c3347
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-20 17:26:55 -0700

    Fuck this shit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-/*
- *hELLO  world
- */
 #include<stdio.h>
+/**
+ *main - Entry point...
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\ "Programming is like building a multilingual puzzle");
+
+return(0);
 }
-I
+
