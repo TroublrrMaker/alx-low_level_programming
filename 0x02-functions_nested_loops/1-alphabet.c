8ebacbee3d78a542daf564e5e72411b8c2828541
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-22 13:59:41 -0700

    who can i trust

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
  */
 void print_alphabet(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-}
-_putchar(i);
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
 }
 _putchar('\n');
 return (0);
