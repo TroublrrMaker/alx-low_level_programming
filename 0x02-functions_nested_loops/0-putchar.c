05251d8d9a1b4a6edd9d8b78482fa9bdd95278e9
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-22 09:10:06 -0700

    Eureka

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+/*
  * Writing a code to print to _putchar to the output
  * -main is the entry point
  *  Program prints "_putchar" to the output
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
- _putchar('_');
- _putchar('p');
- _putchar('u');
- _putchar('t');
- _putchar('c');
- _putchar('h');
- _putchar('a');
- _putchar('r');
- _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
  return (0);
 }
 
