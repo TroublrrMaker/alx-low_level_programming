840d9f10bdfa1cf82c11ac775ec9e7fff391e5e5
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-22 14:26:43 -0700

    I did it again

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 : Prints all alphabets 10times
+ */
+void print_alphabet_x10(void)
+{
+char alphabet;
+int i, count;
+while (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+for (i = 0; count < 10; i++)
+{
+_putchar("%c", alphabet);
+}
+_putchar('\n')
+}
+}
