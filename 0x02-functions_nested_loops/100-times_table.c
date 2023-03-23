f7e2a801f7e3ea9dcc35a84f2898869f3edf5c11
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-22 18:14:26 -0700

    Fall

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,6 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
@@ -41,6 +40,7 @@ _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
+}
 }
 }
 _putchar('\n');
