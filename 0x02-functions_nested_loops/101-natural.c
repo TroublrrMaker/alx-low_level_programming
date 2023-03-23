9fb90d72064d1507a5e295e7b17e40841306e3a0
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-22 17:20:50 -0700

    A time to debug

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,9 +4,8 @@
  */
 
 #include <stdio.h>
-
 /**
- * main - Lists all the natural numbers below 1024 (excluded) that are multiples of 3 or 5.
+ * main - Lists all natural numbers below 1024 exempt multiples of 3 or 5.
  * Return: Always 0.
  */
 int main(void)
