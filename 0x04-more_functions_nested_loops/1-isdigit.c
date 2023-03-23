9cc4508af5ca63a43d453a2668af6e36a2df439e
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-23 09:02:19 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 /**
  * _isdigit - Check the code for digits ranging from 0 to 9
  * @c: is the character to be checked
- * Return: 1 if character is upper, return 1 if char is not upper
+ * Return: 1 if character is upper, return 0 if char is not upper
  */
 
 int _isdigit(int c);
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= '0' && c <= '9')
 	return (1);
 else
 	return (0);
