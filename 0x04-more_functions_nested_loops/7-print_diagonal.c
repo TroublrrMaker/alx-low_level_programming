449dce0115b64071f92d14b9bfd821acd6892b71
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-24 07:58:14 -0700

    My second copy of a commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,18 @@ _putchar('\n');
 }
 else
 {
-int i;
-for (i = 1; i <= n; i++)
+int i, j;
+
+for (i = 0; i < n; i++)
 {
+for (j = 0; j < n; j++)
+{
+if (j == i)
 _putchar('\\');
+else if (j < i)
+_putchar(' ');
 }
 _putchar('\n');
+}
 }
 }
