3b90ca61a30d7480c17e432ed29b64b8f9344c35
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-24 09:02:29 -0700

    Nana Rasta eeeh

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,25 @@
 #include "main.h"
-/**
- * print_square - Function prints the size of a square
- * @size: is the size of the square
- * Return: Always 0
- */
 
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
 void print_square(int size)
+{
 if (size <= 0)
 {
-{
 _putchar('\n');
-}
-
-else
+} else
 {
 int a, b;
-for (a = 0; a <= size; a++)
+
+for (a = 0; a < size; a++)
 {
-for (b = 0; b <= size; j++)
+for (b= 0; b < size; b++)
 {
 _putchar('#');
 }
-_putchar('\n')
+_putchar('\n');
 }
 }
 }
-
-
