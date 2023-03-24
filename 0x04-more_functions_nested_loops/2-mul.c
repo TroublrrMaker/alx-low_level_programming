f595bece3b2519046764ba866a8b8de0fc9db4ce
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-24 03:42:19 -0700

    File 2 updated

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,11 @@
  * mul - function is used to multiply two integers
  * @a : is the first integer to be multiplied
  * @b : is the second integer to be worked on
- * Return : res is the return value of the function mul
+ * Return: the program returns mul
  */
 int mul(int a, int b)
 {
 int res;
-res = a * b;
-return (res);
+res  = a * b;
+return (re);
 }
