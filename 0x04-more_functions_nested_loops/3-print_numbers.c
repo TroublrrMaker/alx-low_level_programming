44f743ecc66e75a00ad7015cb26ac5df2ce8c682
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-24 03:56:33 -0700

    Done with file 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers- function is used to print the digit; 0 to 9
+ * Return: the program has no return it is void
+ */
+void print_numbers(void);
+{
+int i;
+
+if(i < 1; i <= 9; i++)
+{_putchar(i);
+}
+-putchar('\n');
+}
