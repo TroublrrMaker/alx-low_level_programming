6ce30ee9285f11b4e8fbee34521c04b1f545a7e4
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-24 05:29:55 -0700

    You messed up bettygit add 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,16 @@
  */
 void print_most_numbers(void)
 {
- int i;
-
- for (i = 48; i < 58; i++)
- {
- if (i != 50)
- {
- if (i != 52)
- {
- _putchar(i);
- }
- }
- }
- _putchar('\n');
+int i;
+for (i = 48; i < 58; i++)
+{
+if (i != 50)
+{
+if (i != 52)
+{
+_putchar(i);
+}
+}
+}
+_putchar('\n');
 }
