4a95949e7712782466a469bd3528652d5fce8928
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-24 05:27:23 -0700

    this machine is messed up

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers- prints digit (0 through 9) except 2 and 4
+ * print_most_numbers - prints digits (0 through 9) except 2 and 4
  *
  * Return: Always 0.
  */
 void print_most_numbers(void)
 {
-int a;
-for(a = 48; a < 58; a++)
-{
-if(a != 50)
-{
-if(a != 52)
-}
-}
-_putchar(a);
-}
-_putchar('\n');
+ int i;
+
+ for (i = 48; i < 58; i++)
+ {
+ if (i != 50)
+ {
+ if (i != 52)
+ {
+ _putchar(i);
+ }
+ }
+ }
+ _putchar('\n');
 }
