0792647bff063be49efa00d7c73c43ead697c063
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-24 05:10:18 -0700

    Party Over

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints digit (0 through 9) excep 2 and 4
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+while (i != 50)
+
+{
+while (i != 52)
+
+_putchar(i);
+}
+}
+}
+_putchar('\n');
+}
