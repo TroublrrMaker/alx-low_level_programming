ab48864a6fd687c0055e992253da7cc5cb416f11
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-24 08:56:51 -0700

    Nana Rasta eeeh

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,12 +14,15 @@ _putchar('\n');
 else
 {
 int a, b;
-for (a = 1; a <= size; a++)
+for (a = 0; a <= size; a++)
 {
-for (b = 1; b <= size; j++)
+for (b = 0; b <= size; j++)
+{
 _putchar('#');
 }
 _putchar('\n')
 }
 }
+}
+
 
