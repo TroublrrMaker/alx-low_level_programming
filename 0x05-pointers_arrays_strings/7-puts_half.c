199a179a07bdee44a4b78c730872a2dbf54d17f4
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-28 15:01:50 -0700

    sleep comes in the lowest

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - Function prints only last half of character inputted
+ * @str: Is the string to be slashed
+ * Return: Always 0
+ */
+
+void puts_half(char *str)
+{
+int n = 0;
+int len, j;
+for (j = 0; str[j] != '\0'; j++)
+len++;
+n = (len / 2);
+if ((len % 2) == 1)
+n = ((len + 1) / 2);
+
+for (j = n; str[j] != '\0', j++)
+_putchar(str[a]);
+_putchar('\n');
+}
