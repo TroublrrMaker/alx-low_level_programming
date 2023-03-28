9c5b31e44f7a116d5c523b530921ce2b126742eb
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-28 09:09:17 -0700

    Put has been putted:)

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - This function prints a string to the output
+ * @str: Is the string to be printed
+ * Return: Void
+ */
+void _puts(char *str)
+{
+_puts("\n", str);
+}
