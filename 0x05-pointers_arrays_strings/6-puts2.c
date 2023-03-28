c50afd90d6ee115b4752ee79567a0c84977ecd65
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-28 14:27:04 -0700

    Even strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts2 - function to print every 'even' character in string
+ * @str: string to print
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+int j = 0;
+char *k = str;
+int l;
+
+while (*k != '\0')
+{
+k++;
+i++;
+}
+j = i - 1;
+for (l = 0; l <= j; l++)
+{
+if (l % 2 == 0)
+{
+_putchar(str[l]);
+}
+_putchar('\n');
+}
+}
