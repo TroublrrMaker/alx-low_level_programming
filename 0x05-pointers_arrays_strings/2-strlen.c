3b8feef5ca5b974d596639e208927c1196ce726f
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-28 08:52:45 -0700

    Another One

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that prints the length of a string
+ * @s: String
+ * Return: always 0
+ */
+int _strlen(char *s)
+{
+_putchar("%zu", _strlen(*s));
+}
