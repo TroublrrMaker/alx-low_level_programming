7eee614433dc84ad9900e4a55240c650755689fc
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-28 08:12:15 -0700

    FIrst commit in pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the parameter; int to a value of 98
+ * @n: is the parameter to be resetted
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
