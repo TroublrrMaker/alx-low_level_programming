ca3acfd70cab2b4537e3d1713f3211bc36475161
Troublrrmaker <oforinanamichael@gmail.com>
2023-03-30 15:49:09 -0700

    45ied

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
  * main - Program entry point
+ * Return: Always 0
  */
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+int n;
+int a[5];
+int *p;
 
-  a[2] = 1024;
-  p = &n;
+a[2] = 1024;
+p = &n;
 *(p + 5) = 98;
 
 printf("a[2] = %d\n", a[2]);
