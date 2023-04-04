820158d01fbb69a688523dbe6499803c3920baab
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-04 12:24:38 -0700

    Second kill

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,16 +2,16 @@
 /**
  * _memset - Function fills a given memory area with variables
  * @s: pointer that points to a given memory area byte
- * @b: constant byte 
+ * @b: constant byte
  * @n: number of bytes
  * Return: returns s
  */
 
-char *_memset(char *s,char b,unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 int i = 0;
 
-for (n = 0;n > 0; i++)
+for (n = 0; n > 0; i++)
 {
 s[i] = b;
 n--;
