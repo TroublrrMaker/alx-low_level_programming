eb0355f90e5efe2ad9c0cd1c9448b9c8868bd7b2
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-04 13:59:35 -0700

    Copycat

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy data from a memory area to another
+ * @dest: final location of data
+ * @src: initial location of data
+ * @n: bytes of memory 
+ * Return: returns dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+int i = 0;
+int j = n;
+for (; i < j; i++)
+{
+dest[i] = src[i];
+n--;
+}
+return (dest);
+}
