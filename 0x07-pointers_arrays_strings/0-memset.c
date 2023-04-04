8afe4c886d2ec1ec6318a26d7c9d7b2e7879db44
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-04 12:13:41 -0700

    First Kill

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - Function fills a given memory area with variables
+ * @s: pointer that points to a given memory area byte
+ * @b: constant byte 
+ * @n: number of bytes
+ * Return: returns s
+ */
+
+char *_memset(char *s,char b,unsigned int n)
+{
+int i = 0;
+
+while (n > 0; i++)
+{
+s[i] = b;
+n--;
+}
+return (s);
+}
