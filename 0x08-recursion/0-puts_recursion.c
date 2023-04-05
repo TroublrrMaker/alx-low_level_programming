03a8079b71605b4b9f669b0c02fb827d0288ef74
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-05 15:31:23 -0700

    new life

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Function that prints a string
+ * @s: string to be printed
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+_puts(s);
+
+return (0);
+}
