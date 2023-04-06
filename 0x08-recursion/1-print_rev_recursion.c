d790fc177c19bd6e67015fd6f513dc178050d64f
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-06 07:31:15 -0700

    esrever

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev_recursion - Function to print a string in reverse
+ * _print_rev_recursion - Function to print a string in reverse
  * @s: string to be printed
  * Return: Always 0
  */
@@ -10,5 +10,5 @@ void _print_rev_recursion(char *s)
 {
 if (*s != '\0')
 _putchar(*s);
-_print_rev_recursion(s--);
+_print_rev_recursion(s + 1);
 }
