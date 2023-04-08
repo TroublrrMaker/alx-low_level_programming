9fb9b2f2551f4432845c20d1259c5306ec4d80eb
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-08 11:09:41 -0700

    cp

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Programme entry points
+ * @argc: number of arguments
+ * @argv: Arguments array
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc;
+return (0);
+}
