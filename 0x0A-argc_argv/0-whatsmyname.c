99c540cf13fd0a76a6fe861f8d597faf1f03ffa6
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-08 09:49:50 -0700

    Love yall

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Programme entry point
+ * @argc: Number of arguments
+ * @argv: Arguments array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d. %s\n", argc, argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is command line argument projects
