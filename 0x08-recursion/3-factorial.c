541356ea0ba09e96616dc4790b73c5682537ef62
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-07 19:13:12 -0700

    Eureka

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,20 +7,18 @@
 int factorial(int n)
 {
 if (n == 0)
-{
 return (1);
-}
+
 if (n < 0)
-{
 return (-1);
-}
-else if (n != 1)
-{
+
+if (n != 1)
 return (n * (n - 1));
-}
-else if (n == 1)
+
+if (n == 1)
 {
 return (1);
 }
-return (0);
+return 0;
 }
+
