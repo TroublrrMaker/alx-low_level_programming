186cb6c4e01165ddee79a73ebc7f1e2368a8a218
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-08 11:27:03 -0700

    Let it flow

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,15 @@
  * main - Programme entry points
  * @argc: number of arguments
  * @argv: Arguments array
+ * @i: iteration
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-(void)argv;
-printf("%d\n", argc);
+int i;
+for (i = 1; i < argc; i++)
+{
+printf("%s\n %d\n", argv[i], argc);
+}
 return (0);
 }
