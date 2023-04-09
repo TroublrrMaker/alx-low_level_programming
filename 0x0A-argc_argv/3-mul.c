db7c815e0a334bfc32f1628f86ee97b0b0634102
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-09 07:39:20 -0700

    Continuity

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Programme entry points
+ * @argc: number of arguments
+ * @argv: Arguments array
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int mul = 0;
+(void)argc;
+if (argc == 3)
+{
+mul = atoi(argv[1]) * ( atoi(argv[2]));
+printf("%d\n", mul);
+}
+else 
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
