0d537bbc3c51b3f854930db2a33495b54c2c671c
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-11 16:37:51 -0700

    First Blood

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,14 +1,29 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * create_array - creates a character array and init with  specific char
  * @size: size parameter
  * @c: Array of Char
- * Return: Always 0
+ * Return: pointer value if success and NULL if failed
  */
 
 char *create_array(unsigned int size, char c)
 {
+char *str;
+unsigned int i;
 
-return (0);
+str = malloc(sizeof(char) * size);
+if (size == 0)
+{
+return NULL;
+}
+else if (*str == '\0')
+{
+return NULL;
+}
+for (i = 0; i < size; i++)
+{
+str[i] = c;
+}
+return (str);
 }
