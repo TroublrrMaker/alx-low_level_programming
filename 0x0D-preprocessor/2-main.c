7c1e5ae637f0f18337657d29a4163b8ebf8ac81d
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-19 07:56:12 -0700

    My name is ...

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the program
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("The name of the file compiled from is: %s\n", __FILE__);
+return (0);
+}
