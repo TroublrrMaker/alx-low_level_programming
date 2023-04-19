b26ab24f1968d82af5e20e4ecf2a64008d757ce5
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-19 12:48:17 -0700

    Quick Fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,12 +13,6 @@ if (d->name == NULL)
 {
 d->name = "(nil)";
 }
-/**
-* if (d->age == 0)
-* {
-* d->age = "(nil)";
-* }
-*/
 if (d->owner == NULL)
 {
 d->owner = "(nil)";
@@ -28,5 +22,5 @@ if (d == NULL)
 {
 return;
 }
-printf("Name: %s\nAge: %f\nOwner: %s\n",d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
