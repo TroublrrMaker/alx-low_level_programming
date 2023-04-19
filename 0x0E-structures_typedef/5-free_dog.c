2f0040f18f17409f0e319e1997b7ad6ea64a82c5
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-19 13:13:22 -0700

    Final Show

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees mem of struct dog
+ * @d: struct dog
+ */
+
+void free_dog(dog_t *d)
+{
+while (d)
+{
+free(d->name);
+free(d->owner);
+free(d->age);
+free(d);
+}
+}
