b1395710fd9fd251bf2db9706b737eafdda6774f
Troublrrmaker <oforinanamichael@gmail.com>
2023-04-19 10:55:13 -0700

    I did it again

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - Function init the struct; dog
+ * @d: dog variable
+ * @name: dog variable
+ * @age: dog variable
+ * @owner: dog variable
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
